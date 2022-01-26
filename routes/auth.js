02e4b30b0817d1d732d8520818b2e5c0f815f63b
Pritam <pritamdebnath297@gmail.com>
2022-01-26 22:01:05 +0530

    feat: added validation for user creation

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,21 @@
 import express from "express";
 import { Router } from "express";
 import Users from "../model/user.js";
+
+import Joi from "@hapi/joi";
+
+const schema = Joi.object({
+  name: Joi.string().min(4).required(),
+  email: Joi.string().min(6).required().email(),
+  password: Joi.string().min(8).required(),
+});
+
 const router = Router();
+
 router.post("/register", async (req, res) => {
+  const { error } = schema.validate(req.body);
+  if (error) return res.send(error.details[0].message);
+
   const { name, email, password } = req.body;
   const user = new Users({
     name: name,
